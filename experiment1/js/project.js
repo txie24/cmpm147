50a8c7237b8d47cab977e194eaf90c0d9309042c
Tinyu X <txie24@ucsc.edu>
2025-04-07 19:46:27 -0700

    combined code

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -31,4 +31,50 @@ function main() {
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
+
+const fillers = {
+  crafter: ["Dude", "Bro", "Steve", "Alex", "Minin'", "Blockhead"],
+  salutation: ["Yo", "Listen up", "Heads up", "Behold", "Oi"],
+  material: ["dirt", "cobblestone", "rotten stick", "mossy cobblestone", "stale bread", "mysterious blob"],
+  quantity: ["one", "two", "three", "a couple of", "an absurd amount of"],
+  tool: ["pickaxe", "sword", "shovel", "axe", "hoe"],
+  quality: ["crappy", "shitty", "laggy", "glitchy", "disastrous"],
+  action: ["smash", "combine", "pile", "waste", "squish"],
+};
+
+const template = `$crafter, $salutation!
+
+To craft a $quality $tool in Minecraft, you must first $action $quantity $material and then $action another $quantity $material on a crafting table.
+Place them in a bungled pattern that defies logic, and pray to the pixel gods for a half-decent result.
+But chances are—your crafted item will be so $quality, even Creepers will laugh at it!`;
+
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
+  }
+}
+
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
+
+  /* global box */
+  box.innerText = story;
+}
+
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
